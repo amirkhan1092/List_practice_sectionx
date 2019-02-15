3ae036894b44d2099c9b99d9e0d78679f2be80fd
amirkhan1092 <amirkhan1092@gmail.com>
2019-02-15 15:42:06 +0530

    initial commit

---
+++ list_example_string_frequency.py
@@ -0,0 +1,13 @@
+a='hello python world'
+h=[]#init blank list
+st='' #init blank string
+
+for k in a:
+    num=a.count(k)
+    if k in st:
+        h.append(None)
+    else:
+        st +=k
+        h.append(num)
+print('string',list(a))
+print('list ',h)
